f7720f1ba21c684b7c9d94f4807a216c185300d8
ArivuAjay <arivazhagan.pandi@arkinfotec.com>
2022-07-07 15:55:55 +0530

    -- Updated as Repository Pattern

--- routes/api.php
+++ routes/api.php
@@ -19,12 +19,8 @@ Route::prefix('/v1/devsafe/')->group(function () {
     Route::get('healthz', [HealthController::class, 'healthz']);
     Route::get('json', [HealthController::class, 'json']);
 
-    // Sample Repository Pattern
-    Route::get('options', [OptionController::class, 'index']);
-    Route::get('options/{id}', [OptionController::class, 'show']);
-    Route::post('options', [OptionController::class, 'store']);
-    Route::put('options/{id}', [OptionController::class, 'update']);
-    Route::delete('options/{id}', [OptionController::class, 'delete']);
-
     // Write Your Service Routes
+    Route::apiResources([
+        'options' => OptionController::class
+    ]);
 });
